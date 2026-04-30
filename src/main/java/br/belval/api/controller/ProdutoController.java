5cee2654fd7a72e3385588b51dc97d50c440a6ba
Pedro <rm101619@estudante.fieb.edu.br>
2026-04-29 22:51:17 -0300

    isso ai papae

--- src/main/java/br/belval/api/controller/ProdutoController.java
+++ src/main/java/br/belval/api/controller/ProdutoController.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,18 +16,31 @@ import br.belval.api.repository.ProdutoRepository;
 @RestController
 public class ProdutoController {
 	
+	
+
+
 	@Autowired
 	private ProdutoRepository repository;
+	
+	
+	
+	/**
+	 * Retorna todos produtos
+	 * @return
+	 */
+	
+	@GetMapping("/produtos")
 	public ResponseEntity<Iterable<Produto>> obterProdutos() {
-		
+		return ResponseEntity
+				.status(HttpStatus.OK)
+				.body(repository.findAll());
+				
 	}
 	
 	//curl POST http://localhost:8080/produtos -H "Content-Type: application/json; Charset=utf-8" -d @produto-pao.json
 	@PostMapping("/produtos")
 	public ResponseEntity<Produto> criarProduto(@RequestBody Produto produto) {
-		return ResponseEntity.status(HttpStatus.OK)
-				.body(repository.findAll())
-				}
+
 		produto.setDataCriacao(LocalDateTime.now());
 		
 		System.out.println("Chegou : " + produto.toString());
